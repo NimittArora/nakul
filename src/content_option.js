692d32f4fe958cc30f26c13ea2449c5921c54b0a
Nimitt Arora <nimittarora03@gmail.com>
2024-04-11 20:27:08 +0530

    second commit

---
+++ src/content_option.js
@@ -0,0 +1,180 @@
+const logotext = "Nimitt Arora";
+const meta = {
+    title: "Nimitt Arora",
+    description: "I’m Nimitt Arora , A 3rd year CS undergraduate at BITS Pilani Goa Campus",
+};
+
+const introdata = {
+    title: "I’m Nimitt",
+    animated: {
+        first: "I love coding",
+        second: "I love music",
+        third: "I love sports",
+    },
+    description: "I’m Nimitt Arora , A 3rd year CS undergraduate at BITS Pilani Goa Campus",
+    your_img_url: "/newpic.jpg",
+};
+
+const dataabout = {
+    title: "Education",
+    aboutme: "I am currently pursuing Bachelors in Computer Science Engineering at BITS Pilani Goa Campus .I have passed 12th CBSE from DAV Public School Pune with 95.2%.I have passed 10th CBSE from Indira National School Pune with 95%.",
+};
+const dataabout2 = {
+    title: "Work Experience",
+    aboutme: "Machine Learning Intern at VoiceQube - Collaborated with a team to develop an advanced Generative AI engine called Knowledge Pool, designed to generate multiple sections of Data Structures and Algorithms (DSA) and Computer Science subject questions (including MCQs) for student practice using OpenAI API implementation in MERN stack.Implemented an auto-vetting system to test the complexity level of each generated question and segregated them based on relevance.Developed database systems (MongoDB) to efficiently store the generated questions, ensuring easy access for different users. Incorporated a user login functionality to allow authorized access to the question database.Ensured that each generated question had corresponding solutions and test cases when necessary.Solution dockerized and deployed currently being used by 3 colleges to generate DSA Questions. 20+ DSA Questions generated and 50+ MCQ Questions generated.",
+};
+const dataabout3 = {
+    title: "Position of responsibility",
+    aboutme: "Currently leading a 80-member team at IEEE BITS Goa, overseeing impactful quizzes, contests, workshops, and talks. Led the student organizing committee for the 2nd International IEEE Applied Sensing Conference, featuring Somnath S., Chairman ISRO.Successfully pioneered a highly successful 6-month partnership with Coding Ninjas."
+};
+const worktimeline = [{
+        jobtitle: "JEE Mains",
+        where: "99.8 Percentile",
+        date: "2021",
+    },
+    {
+        jobtitle: "BITSAT",
+        where: "363/450",
+        date: "2021",
+    },
+    {
+        jobtitle: "JEE Adv",
+        where: "AIR 4140",
+        date: "2021",
+    },
+];
+
+const skills = [{
+        name: "Data Structures and Algorithms",
+        value: 95,
+    },
+    {
+        name: "Database Management Systems",
+        value: 85,
+    },
+    {
+        name: "Competitive Programming",
+        value: 85,
+    },
+    {
+        name: "Frontend Web Development",
+        value: 80,
+    },
+    {
+        name: "Machine Learning",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "BITS GOA CRICKET TEAM",
+        description: "Represented BITS Goa  in matches against national colleges . Won silver medal in spree 2024. Won bronze medal in inter-bits 2024",
+    },
+    {
+        title: "BITS GOA TABLE TENNIS TEAM",
+        description: "Represented BITS Goa in matches against national colleges . Won gold medal in IIIT Bangalore 2023. Won silver medal in inter-bits 2024",
+    },
+    {
+        title: "CORE MEMBER-CENTER FOR TECHNICAL EDUCATION, BITS GOA",
+        description: "Managed the website of CTE BITS Goa from june 2022 to june 2023",
+    },
+    {
+        title: "AMATEUR GUITARIST",
+        description: "I play acoustic guitar for recreation",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "nimittarora03@gmail.com",
+    YOUR_FONE: "+91 9325958850",
+    description: "For queries you can mail me on the above email id or dm me on the above phone no.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataabout2,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+    dataabout3,
+};
